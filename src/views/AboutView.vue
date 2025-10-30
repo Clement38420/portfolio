c69e5b9bb05c5dffe78d961bc8ab641b62cb86f3
Clément Charbonnel <c.charbo38@gmail.com>
2025-10-30 16:12:38 +0100

    fix(AboutView): update gradient colors for studies SVG
    The gradient starting color was white and not transparent.

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -129,7 +129,7 @@ onMounted(() => {
           <svg width="40" height="400" viewBox="0 0 10 100" class="studies-svg">
             <defs>
               <linearGradient id="gradientStroke" x1="0%" y1="100%" x2="0%" y2="0%">
-                <stop offset="0%" stop-color="#ffffffff" />
+                <stop offset="0%" style="stop-color: #1d4ed8; stop-opacity: 0" />
                 <stop offset="40%" stop-color="#1d4ed8" />
                 <stop offset="100%" stop-color="#1d4ed8" />
               </linearGradient>
